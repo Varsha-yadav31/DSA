e68ca820882acfd72051b5b66962505e031592db
varsha <152709598+Varsha-yadav31@users.noreply.github.com>
2026-01-23 19:52:57 +0530

    closestno

--- table.cpp
+++ table.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    int n;
-    cin >> n;
-
-    for (int i = 1; i <= 10; i++) {
-        cout << n << " * " << i << " = " << n * i << endl;
+/*int main(){
+    int n ;
+    cin>>n;
+    for(int i = 1; i <= 10; i++){
+        cout<< n << "*" << i << "=" << n * i<<endl;
     }
+}
+*/
 
-    return 0;
+void table(int n ,int i = 1){
+   if(i==11)
+    return;
+    cout<< n << "*" << i << "=" << n * i<<endl;
+    i++;
+    table(n,i);
 }
 
+int main(){
+    int n = 5;
+    table(n);
+    
+}

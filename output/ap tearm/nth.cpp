64d4aaa8c2885d9922be7558d8915c780699113b
varsha <152709598+Varsha-yadav31@users.noreply.github.com>
2026-01-27 10:28:09 +0530

    update nth term logic

--- output/ap tearm/nth.cpp
+++ output/ap tearm/nth.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 int main() {
     int a, b, n;
+    int nthTerm;
     
     cin >> a >> b >> n;   // input values
     
     int d = b - a;       // common difference
-    int nthTerm = a + (n - 1) * d;
     
     cout << nthTerm;
     
     return 0;
 }
+

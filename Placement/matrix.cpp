f011e441de89b7ff843c9650e8b41110e18f6d18
varsha <152709598+Varsha-yadav31@users.noreply.github.com>
2026-06-30 12:23:35 +0530

    Solved Two Sum using Two Pointers and practiced Linked List operations

--- Placement/matrix.cpp
+++ Placement/matrix.cpp
@@ -1,4 +1,8 @@
-nclass Solution {
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
 
@@ -15,17 +19,31 @@ public:
             int row = mid / n;
             int col = mid % n;
 
-            if (matrix[row][col] == target) {
+            if (matrix[row][col] == target)
                 return true;
-            }
-            else if (matrix[row][col] < target) {
+
+            else if (matrix[row][col] < target)
                 st = mid + 1;
-            }
-            else {
+
+            else
                 end = mid - 1;
-            }
         }
 
         return false;
     }
 };
+
+int main() {
+
+    vector<vector<int>> matrix = {
+        {1,3,5,7},
+        {10,11,16,20},
+        {23,30,34,60}
+    };
+
+    Solution obj;
+
+    cout << obj.searchMatrix(matrix, 3);
+
+    return 0;
+}

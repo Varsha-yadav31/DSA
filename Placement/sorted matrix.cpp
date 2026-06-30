96a0fb6f36c3efaf699fdd25ae56e12cf2aa3888
varsha <152709598+Varsha-yadav31@users.noreply.github.com>
2026-06-30 12:24:47 +0530

    Solved matrix search, sorted matrix, vector basics and Two Sum in C++

--- Placement/sorted matrix.cpp
+++ Placement/sorted matrix.cpp
@@ -1,20 +1,34 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-bool sortedMatrix(int mat , int x){
+
+bool sortedMatrix(vector<vector<int>>& mat, int x) {
+
     int n = mat.size();
     int m = mat[0].size();
-    for(int i = 0 ; i<n; i++){
-        for(int j = 0; j<m; j++){
-            if(mat[i][j]== x){
-                return 1;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (mat[i][j] == x) {
+                return true;
             }
         }
-        return 0;
     }
 
-};
-int main(){
-    vector<int>mat = {2 3 4 5 6 7 8 9 };
+    return false;
+}
+
+int main() {
+
+    vector<vector<int>> mat = {
+        {2, 3, 4},
+        {5, 6, 7},
+        {8, 9, 10}
+    };
+
     int x = 6;
-    cout<<mat;
+
+    cout << sortedMatrix(mat, x);
+
+    return 0;
 }
